62c4d2a655c34cb5d638cdad9e560b112d2eefab
chris <chunghao777@gmail.com>
2024-03-24 10:12:58 +0800

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:SayAnything/router/router_config.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+   HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
 }
 
@@ -24,5 +27,14 @@ class MyApp extends StatelessWidget {
       routeInformationProvider: router.routeInformationProvider,
       routerDelegate: router.routerDelegate,
     );
+  }
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+     ..badCertificateCallback =
+         (X509Certificate cert, String host, int port) => true;
   }
 }
